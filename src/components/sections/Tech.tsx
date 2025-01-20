4c5de6449c65ae9f02a71d63b9ce1366d16e742d
noushal <noushaluliyannoor7@gmail.com>
2025-01-20 12:08:52 +0530

    Phone View Updated

--- src/components/sections/Tech.tsx
+++ src/components/sections/Tech.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { BallCanvas } from "../canvas";
 import { SectionWrapper } from "../../hoc";
 import { technologies } from "../../constants";
