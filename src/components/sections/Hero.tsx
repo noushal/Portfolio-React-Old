50982c1e847d45377857217d47ed9619c4e937d1
noushal <noushaluliyannoor7@gmail.com>
2025-01-20 12:27:52 +0530

    Phone View Updated

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -20,24 +20,24 @@ const Hero = () => {
     return (
       <section className={`relative mx-auto h-screen w-full`}>
         <div
-          className={`absolute inset-0 top-[120px] mx-auto max-w-7xl ${styles.paddingX} flex flex-row items-start gap-5`}
+          className={`absolute inset-0 mx-auto max-w-7xl ${styles.paddingX} flex flex-row items-start`}
         >
           <div className="mt-5 flex flex-col items-center justify-center">
-            <div className="h-5 w-5 rounded-full bg-[#915EFF]" />
-            <div className="violet-gradient h-40 w-1 sm:h-80" />
+            <div className="h-4 w-4 rounded-full bg-[#915EFF]" />
+            <div className="violet-gradient h-32 w-[2px]" />
           </div>
   
           <div>
-            <h1 className={`${styles.heroHeadText} text-white`}>
+            <h1 className={`${styles.heroHeadText} text-white leading-tight`}>
               Hi, I'm <span className="text-[#915EFF]">{config.hero.name}</span>
             </h1>
-            <p className={`${styles.heroSubText} text-white-100 mt-2`}>
-              {config.hero.p[0]} <br className="hidden sm:block" />
+            <p className={`${styles.heroSubText} text-white-100 mt-1`}>
+              {config.hero.p[0]}
+              <br />
               {config.hero.p[1]}
             </p>
           </div>
         </div>
-        
       </section>
     );
   }else{
