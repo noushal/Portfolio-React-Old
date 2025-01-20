2ac1c9363d2784b4ce35ce2fb72453ee6da773b4
noushal <noushaluliyannoor7@gmail.com>
2025-01-20 12:08:11 +0530

    Phone View Updated

--- src/components/sections/Tech.tsx
+++ src/components/sections/Tech.tsx
@@ -1,18 +1,36 @@
+import React, { useState, useEffect } from "react";
 import { BallCanvas } from "../canvas";
 import { SectionWrapper } from "../../hoc";
 import { technologies } from "../../constants";
 
 const Tech = () => {
-  return (
-    <>
-      <div className="hidden sm:flex sm:flex-row sm:flex-wrap sm:justify-center sm:gap-10">
-        {technologies.map((technology) => (
-          <div className="h-28 w-28" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
-          </div>
-        ))}
+  const [isLowEnd, setIsLowEnd] = useState(false);
+
+  useEffect(() => {
+    const isLowEndDevice = () => {
+      const ua = navigator.userAgent || navigator.vendor;
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua);
+    };
+
+    setIsLowEnd(isLowEndDevice());
+  }, []);
+
+  if (isLowEnd) {
+    return (
+      <div className="flex justify-center">
+        <p>3D models are not supported on this device.</p>
       </div>
-    </>
+    );
+  }
+
+  return (
+    <div className="hidden sm:flex sm:flex-row sm:flex-wrap sm:justify-center sm:gap-10">
+      {technologies.map((technology) => (
+        <div className="h-28 w-28" key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
   );
 };
 
